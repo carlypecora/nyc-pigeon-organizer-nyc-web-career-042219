d58d92af834a88422ec93f001a7c68498c7d913b
Carly Pecora <carly@carlypecora.com>
2019-04-12 18:49:54 -0400

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+	pigeon_list = {}
+	data.each do |quality_key, values|
+		values.each do |quality_value, bird_names|
+			for bird_name in bird_names
+				if !pigeon_list[bird_name]
+					pigeon_list[bird_name] = {}
+				end
+				if !pigeon_list[bird_name][quality_key]
+					pigeon_list[bird_name][quality_key] = []		
+				end		
+				pigeon_list[bird_name][quality_key].push(quality_value.to_s)	
+			end
+		end
+	end
+	pigeon_list
 end
